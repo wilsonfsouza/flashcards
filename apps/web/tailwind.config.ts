522f1ca10ac30ebe19d9b95e0dbe4b51aeafa864
Wilson Franca <wilson.franca.92@gmail.com>
2023-11-19 13:44:13 -0800

    feat(apps/web): homepage

--- apps/web/tailwind.config.ts
+++ apps/web/tailwind.config.ts
@@ -16,7 +16,7 @@ const config: Config = {
     './src/app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
-    colors,
+    colors: { ...colors, current: 'currentColor' },
     fontFamily: {
       sans: fonts.default,
     },
